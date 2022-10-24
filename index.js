c1ff8125b43845ed4306ce95db6eb969e2229628
Your Name <you@example.com>
2022-10-24 23:12:09 +0600

    get data by id

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
+app.get("/course/:id",(req,res)=> {
+  const id = req.params.id;
+  // console.log(id);
+  const selectedCourse = courses.find(c => c.id == id)
+  if(!selectedCourse){
+    res.send('your data is not found')
+  }
+   res.send(selectedCourse)
+})
+
 app.listen(Port, () => {
   console.log("Server is running", Port);
 });
